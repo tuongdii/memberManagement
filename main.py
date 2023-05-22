5d770d4a4303d17a84211cee658ceb7068c5431c
V.T.TuongDi <vothituongduy20@gmail.com>
2023-05-22 15:10:16 +0700

    add page details

--- main.py
+++ main.py
@@ -1,24 +1,64 @@
 from PyQt6 import QtWidgets as qtw
 from PyQt6 import uic
 import sys
+import re
 
+page = 1
 class Main(qtw.QMainWindow):
     def __init__(self):
         super(Main, self).__init__()
         uic.loadUi("main.ui", self)
+        self.btn_view1.clicked.connect(self.updatePage)
+        self.btn_viewImg1.clicked.connect(self.updatePage)
         self.btn_view1.clicked.connect(self.viewDetail)
         self.btn_viewImg1.clicked.connect(self.viewDetail)
-
+        
+        self.btn_view2.clicked.connect(self.updatePage)
+        self.btn_viewImg2.clicked.connect(self.updatePage)
+        self.btn_view2.clicked.connect(self.viewDetail)
+        self.btn_viewImg2.clicked.connect(self.viewDetail)
+       
+        self.btn_view3.clicked.connect(self.updatePage)
+        self.btn_viewImg3.clicked.connect(self.updatePage)
+        self.btn_view3.clicked.connect(self.viewDetail)
+        self.btn_viewImg3.clicked.connect(self.viewDetail)
+        
+        self.btn_view4.clicked.connect(self.updatePage)
+        self.btn_viewImg4.clicked.connect(self.updatePage)
+        self.btn_view4.clicked.connect(self.viewDetail)
+        self.btn_viewImg4.clicked.connect(self.viewDetail)
+        
+        self.btn_view5.clicked.connect(self.updatePage)
+        self.btn_viewImg5.clicked.connect(self.updatePage)
+        self.btn_view5.clicked.connect(self.viewDetail)
+        self.btn_viewImg5.clicked.connect(self.viewDetail)
+        
+        self.btn_view6.clicked.connect(self.updatePage)
+        self.btn_viewImg6.clicked.connect(self.updatePage)
+        self.btn_view6.clicked.connect(self.viewDetail)
+        self.btn_viewImg6.clicked.connect(self.viewDetail)
         
     def viewDetail(self):
         self.detail = Detail()
         self.detail.show()
+        
+    def updatePage(self):
+        global page
+        button_name = self.sender().objectName()
+        page_number = re.search(r'\d+', button_name)
+        if page_number:
+            page = int(page_number.group())
+        
 
+        
 class Detail(qtw.QMainWindow):
     def __init__(self):
         super(Detail, self).__init__()
-        uic.loadUi("details.ui", self)
+        pageName = "details" + str(page) + ".ui"
+        uic.loadUi(pageName, self)
         self.btn_back.clicked.connect(lambda:self.close())
+
+
                
 if __name__ == '__main__':
     app = qtw.QApplication(sys.argv)
